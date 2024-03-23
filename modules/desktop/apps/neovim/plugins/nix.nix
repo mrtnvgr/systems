debea94e798efc9e8d68a3a500807b7dcc4dd308
mrtnvgr <root@unixis.fun>
2024-03-23 08:27:38 +0700

    fix: enable nixd lsp server

--- modules/desktop/apps/neovim/plugins/nix.nix
+++ modules/desktop/apps/neovim/plugins/nix.nix
@@ -6,7 +6,6 @@ in {
   home-manager.users.${user}.programs.nixvim.plugins = mkIf cfg.enable {
     hmts.enable = true;
 
-    # TODO: https://github.com/nix-community/nixd/issues/357
-    # lsp.servers.nixd.enable = true;
+    lsp.servers.nixd.enable = true;
   };
 }
