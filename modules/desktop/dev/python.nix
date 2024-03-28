855f16488908a93df1824ed0f9f707eba1902094
mrtnvgr <root@unixis.fun>
2024-03-28 15:04:57 +0700

    fix(python/ml): repair natasha pkg

--- modules/desktop/dev/python.nix
+++ modules/desktop/dev/python.nix
@@ -2,6 +2,15 @@
 let
   inherit (lib) mkIf mkEnableOption;
 
+  # TODO: https://github.com/NixOS/nixpkgs/pull/298964
+  natashaPkgs = import (pkgs.fetchFromGitHub {
+    owner = "mrtnvgr";
+    repo = "nixpkgs";
+    rev = "fix/razdel";
+    hash = "sha256-7hOemLE4VditQUd4FrybCaV57RkJIOtUf6rlpDzJvrc=";
+  }) { system = pkgs.stdenv.system; };
+  natasha = natashaPkgs.python3Packages.natasha;
+
   pypkgs = ps: with ps; [
     # Essentials
     requests
@@ -12,6 +21,10 @@ let
 
     # Web scraping
     beautifulsoup4
+
+    # Notebooks
+    jupyter
+    ipython
 
     # Machine learning
     numpy
