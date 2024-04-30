c4b143dfb2da76a226ceed6d2329d8d9dee3bfa6
mrtnvgr <root@unixis.fun>
2024-04-30 08:00:43 +0700

    chore!(core): change PS1

--- core/shell.nix
+++ core/shell.nix
@@ -20,7 +20,10 @@ in {
     programs.bash = {
       enable = true;
 
-      initExtra = ''PS1="${icon} \W ${reset}"'';
+      # initExtra = ''PS1="${icon} \W ${reset}"'';
+      initExtra = ''
+        PS1="\[$(tput bold)\]\[$(tput setaf 1)\][\[$(tput setaf 3)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 5)\]\W\[$(tput setaf 1)\]]\[$(tput setaf 7)\]\\$ \[$(tput sgr0)\]"
+      '';
 
       shellAliases = {
         perm = "stat -c \"%a %n\"";
