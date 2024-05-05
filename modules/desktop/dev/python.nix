86b2e03f42ff7ecaaa3560aa87ad78df300fa6fa
mrtnvgr <root@unixis.fun>
2024-05-05 18:44:29 +0700

    chore(desktop/dev/python): remove TODOs

--- modules/desktop/dev/python.nix
+++ modules/desktop/dev/python.nix
@@ -2,15 +2,6 @@
 let
   inherit (lib) mkIf mkEnableOption;
 
-  # TODO: https://github.com/NixOS/nixpkgs/pull/298964
-  # natashaPkgs = import (pkgs.fetchFromGitHub {
-  #   owner = "mrtnvgr";
-  #   repo = "nixpkgs";
-  #   rev = "fix/razdel";
-  #   hash = "sha256-7hOemLE4VditQUd4FrybCaV57RkJIOtUf6rlpDzJvrc=";
-  # }) { system = pkgs.stdenv.system; };
-  # natasha = natashaPkgs.python3Packages.natasha;
-
   pypkgs = ps: with ps; [
     # Essentials
     requests
@@ -37,8 +28,8 @@ let
     # transformers
     # scikit-learn
 
-    # FIXME: pygame
-    # FIXME: pyppeteer
+    # pygame
+    # pyppeteer
   ];
 
   cfg = config.modules.desktop.dev.python;
@@ -47,9 +38,7 @@ in
   options.modules.desktop.dev.python.enable = mkEnableOption "python";
   config = mkIf cfg.enable {
     home-manager.users.${user} = {
-      home.packages = with pkgs; [
-        (pkgs.python3.withPackages pypkgs)
-      ];
+      home.packages = [ (pkgs.python3.withPackages pypkgs) ];
     };
   };
 }
