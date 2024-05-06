6b97fc786c18c36580165106f2f3654ec0b1442d
mrtnvgr <root@unixis.fun>
2024-05-06 17:27:15 +0700

    chore: update dependencies

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     waybar.url = "github:Alexays/waybar";
 
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
