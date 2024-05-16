bdfda701157237dc221664e2d976ca1c29503a3a
mrtnvgr <root@unixis.fun>
2024-05-16 18:09:48 +0700

    feat(core): disable auto gc

--- core/nix.nix
+++ core/nix.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, config, lib, ... }: {
+{ pkgs, ... }: {
   nix = {
     # Use new experimental nix
     package = pkgs.nixFlakes;
@@ -7,20 +7,12 @@
       # Enable flakes
       experimental-features = "nix-command flakes";
 
-      # Deduplicate store
+      # Hard link store content
       auto-optimise-store = true;
 
       # Disable "uncommited changes" warning
       warn-dirty = false;
     };
-
-    # Auto garbage-collection
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-
   };
 
   # Faster rebuilds
