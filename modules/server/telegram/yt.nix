9bde1c5507e37451daf6937d130c8528d169602f
mrtnvgr <martynovegorOF@yandex.ru>
2025-05-15 22:28:11 +0700

    feat(server/telegram/yt): use public repository

--- modules/server/telegram/yt.nix
+++ modules/server/telegram/yt.nix
@@ -9,25 +9,21 @@ let
   service-user = "telegram-yt";
 
   pkg = pkgs.rustPlatform.buildRustPackage {
-    pname = "yt-tg";
-    version = "0.1.0-dev";
+    pname = "yttg";
+    version = "0.0.0";
 
-    # src = pkgs.fetchFromGitHub {
-    #   owner = "mrtnvgr";
-    #   repo = "yt-tg";
-    #   rev = "TODO";
-    #   hash = "";
-    # };
-    src = /home/user/yt-tg;
+    src = pkgs.fetchFromGitHub {
+      owner = "mrtnvgr";
+      repo = "yttg";
+      rev = "d25f00060c558dd58306eada5c1ae761bb1ec34b";
+      hash = "sha256-RsCGrLjplZVY9CqcwUiCI/7csrHNJRndQRXevzmhyq4=";
+    };
 
     nativeBuildInputs = with pkgs; [ pkg-config ];
     buildInputs = with pkgs; [ openssl ];
 
-    ADMIN_ID = toString cfg.adminId;
-
-    # useFetchCargoVendor = true;
-    # cargoHash = "sha256-wQ01AVn5C52GCEACF0de5uPGFpKGjmWJHfw+Oc0Ln/s=";
-    cargoLock.lockFile = /home/user/yt-tg/Cargo.lock;
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-Bfu1VlhloIJQr8VTuMHF64UL3TUZ5VxP5ki/fkre67Q=";
   };
 in {
   options.modules.server.telegram.yt = {
@@ -55,6 +51,8 @@ in {
 
         TELOXIDE_API_URL = mkIf api_cfg.enable "http://0.0.0.0:${toString api_cfg.port}";
         TELOXIDE_TOKEN = cfg.token;
+
+        ADMIN_ID = toString cfg.adminId;
       };
 
       serviceConfig = {
@@ -63,7 +61,7 @@ in {
         Type = "simple";
         NonBlocking = true;
         WorkingDirectory = workdir;
-        ExecStart = "${pkg}/bin/yt-tg";
+        ExecStart = "${pkg}/bin/yttg";
         ExecReload = "/bin/sh kill -HUP \${MAINPID}";
         ExecStop = "/bin/sh kill -INT \${MAINPID}";
         TimeoutSec = 30;
