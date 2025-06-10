271d92d6c2b3f23e43e00dd21ce95d395776dcc6
mrtnvgr <martynovegorOF@yandex.ru>
2025-06-10 13:47:48 +0700

    feat(desktop/core): improve m8sort script

--- modules/desktop/core/packages.nix
+++ modules/desktop/core/packages.nix
@@ -4,7 +4,10 @@ let
   cfg = config.modules.desktop;
 
   m8sort = pkgs.writeShellScriptBin "m8sort" ''
-    fatsort -x Songs "$1"
+    fatsort -X Songs "$1"
+    fatsort -X Bundles "$1"
+    fatsort -t -D Songs "$1"
+    fatsort -t -D Bundles "$1"
   '';
 in {
   config = mkIf cfg.enable {
