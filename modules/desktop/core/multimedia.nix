d1784fb3e2fd37ce7370ab91f2cfa4c2a604a3c7
mrtnvgr <martynovegorOF@yandex.ru>
2025-10-17 19:44:20 +0700

    chore(desktop/core/multimedia): add rubberband

--- modules/desktop/core/multimedia.nix
+++ modules/desktop/core/multimedia.nix
@@ -4,6 +4,6 @@ let
   cfg = config.modules.desktop;
 in {
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ feh mpv ffmpeg imagemagick sox ];
+    environment.systemPackages = with pkgs; [ feh mpv ffmpeg imagemagick sox rubberband ];
   };
 }
