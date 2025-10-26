2c47f8216dceee3331ffcc31aa3b958c446fbd96
mrtnvgr <martynovegorOF@yandex.ru>
2025-10-26 12:26:19 +0700

    chore(hyprpop/foot): remove unnecessary quotes

--- modules/desktop/core/rices/hyprpop/foot/default.nix
+++ modules/desktop/core/rices/hyprpop/foot/default.nix
@@ -15,33 +15,32 @@ in {
             term = "xterm-256color";
           };
 
-          cursor.color = "${palette.background} ${palette.text}";
           mouse.hide-when-typing = "yes";
 
           colors.alpha = theme.opacity;
 
-          # note: modified to personal liking
           colors = {
-            background = "${palette.background}";
-            foreground = "${palette.text}";
+            background = palette.background;
+            foreground = palette.text;
+            cursor = "${palette.background} ${palette.text}";
 
-            regular0 = "${palette.background}";
-            regular1 = "${palette.red}";
-            regular2 = "${palette.green}";
-            regular3 = "${palette.yellow}";
-            regular4 = "${palette.blue}";
-            regular5 = "${palette.violet}";
-            regular6 = "${palette.lavender}";
-            regular7 = "${palette.text}";
+            regular0 = palette.background;
+            regular1 = palette.red;
+            regular2 = palette.green;
+            regular3 = palette.yellow;
+            regular4 = palette.blue;
+            regular5 = palette.violet;
+            regular6 = palette.lavender;
+            regular7 = palette.text;
 
-            bright0 = "${palette.gray2}";
-            bright1 = "${palette.red}";
-            bright2 = "${palette.green}";
-            bright3 = "${palette.yellow}";
-            bright4 = "${palette.blue}";
-            bright5 = "${palette.violet}";
-            bright6 = "${palette.lavender}";
-            bright7 = "${palette.text}";
+            bright0 = palette.gray2;
+            bright1 = palette.red;
+            bright2 = palette.green;
+            bright3 = palette.yellow;
+            bright4 = palette.blue;
+            bright5 = palette.violet;
+            bright6 = palette.lavender;
+            bright7 = palette.text;
           };
         };
       };
