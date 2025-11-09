148f7df39e79c220b5b5b681a8f99b70e8239ae8
mrtnvgr <martynovegorOF@yandex.ru>
2025-11-09 13:19:06 +0700

    feat(core/git): update options

--- core/git.nix
+++ core/git.nix
@@ -1,24 +1,20 @@
 { pkgs, user, ... }: {
+  environment.systemPackages = with pkgs; [ github-cli git-lfs git-crypt ];
+
   home-manager.users.${user} = {
-    home.packages = with pkgs; [ github-cli git-lfs git-crypt ];
     programs.git = {
       enable = true;
-      userName = "mrtnvgr";
-      userEmail = "martynovegorOF@yandex.ru";
+      settings = {
+        user.name = "mrtnvgr";
+        user.email = "martynovegorOF@yandex.ru";
 
-      aliases = {
-        ammit = "commit -a --amend --no-edit";
-        undo = "reset HEAD~1";
+        pull.rebase = true;
+        init.defaultBranch = "master";
       };
 
       signing = {
         signByDefault = true;
         key = "6FADDB43D5A5FE52683509435B3379E981EF48B1";
-      };
-
-      extraConfig = {
-        pull.rebase = true;
-        init.defaultBranch = "master";
       };
     };
   };
