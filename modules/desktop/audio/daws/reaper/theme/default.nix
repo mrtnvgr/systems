f9f22ac1d261f33d218efcad2521909bc7358203
mrtnvgr <martynovegorOF@yandex.ru>
2025-11-29 23:06:19 +0700

    feat(reaper): supply a set of nice custom colors

--- modules/desktop/audio/daws/reaper/theme/default.nix
+++ modules/desktop/audio/daws/reaper/theme/default.nix
@@ -1,7 +1,36 @@
-{ pkgs, lib, config, user, ... }: let
+{ inputs, pkgs, lib, config, user, ... }: let
+  mrtnvgr-lib = inputs.mrtnvgr.lib { inherit pkgs; };
+
   cfg = config.modules.desktop.audio.daws.reaper;
+
+  # inherit (config.colorScheme) palette;
+  colors = [
+    "#906BFA"
+    "#D86BFA"
+    "#EE65CB"
+    "#F36889"
+    "#F3845D"
+    "#F3D161"
+    "#54D362"
+    "#1EDFAD"
+    "#00B6F1"
+    "#ABCDFF"
+    "#D1E4FF"
+  ];
+  bgrColors = map mrtnvgr-lib.colors.hex.flip colors;
+  custColors = lib.concatStrings (map (x: "${x}00") bgrColors);
 in {
   config = lib.mkIf cfg.enable {
+    modules.desktop.audio.daws.reaper.config."reaper.ini" = /* dosini */ ''
+      ; Selected theme
+      [reaper]
+      lastthemefn5=/home/${user}/.config/REAPER/ColorThemes/Reapertips.ReaperTheme
+
+      ; Custom colors
+      [reaper]
+      custcolors=${custColors}
+    '';
+
     home-manager.users.${user} = {
       home.file.".config/REAPER/ColorThemes/Reapertips.ReaperThemeZip".source = pkgs.fetchurl {
         url = "https://github.com/mrtnvgr/reapertips-theme/raw/refs/heads/v1.90/02_Theme/Reapertips%20Theme.ReaperThemeZip";
