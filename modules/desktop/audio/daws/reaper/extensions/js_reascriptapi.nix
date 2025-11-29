39ae75bc60f5449a797303a00b6e4345a2c4f870
mrtnvgr <martynovegorOF@yandex.ru>
2025-11-29 23:04:02 +0700

    chore(reaper): move js_reascriptapi package to nurpkgs

--- modules/desktop/audio/daws/reaper/extensions/js_reascriptapi.nix
+++ modules/desktop/audio/daws/reaper/extensions/js_reascriptapi.nix
@@ -1,15 +1,12 @@
 { lib, config, user, pkgs, ... }:
 let
-  inherit (lib) mkIf;
   cfg = config.modules.desktop.audio.daws.reaper;
-
-  # TODO: move to nurpkgs
-  js_reascriptapi = pkgs.callPackage ./TEMP.nix { };
+  pkg = pkgs.js_ReaScriptAPI;
 in {
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     home-manager.users.${user} = {
-      home.file.".config/REAPER/UserPlugins/reaper_js_ReaScriptAPI64.so".source = "${js_reascriptapi}/UserPlugins/reaper_js_ReaScriptAPI64.so";
-      home.file.".config/REAPER/UserPlugins/reaper_js_ReaScriptAPI64.dll".source = "${js_reascriptapi}/UserPlugins/reaper_js_ReaScriptAPI64.dll";
+      home.file.".config/REAPER/UserPlugins/reaper_js_ReaScriptAPI64.so".source = "${pkg}/UserPlugins/reaper_js_ReaScriptAPI64.so";
+      home.file.".config/REAPER/UserPlugins/reaper_js_ReaScriptAPI64.dll".source = "${pkg}/UserPlugins/reaper_js_ReaScriptAPI64.dll";
     };
   };
 }
