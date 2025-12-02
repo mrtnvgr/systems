100d7a3e4c6b85c3449bef8ababd2a45a30d2012
mrtnvgr <martynovegorOF@yandex.ru>
2025-12-02 09:56:13 +0700

    chore!(core/temp): just clean /tmp every boot, do not use tmpfs

--- core/temp.nix
+++ core/temp.nix
@@ -1,6 +1,6 @@
 # https://github.com/NixOS/nixpkgs/issues/336089#issuecomment-2308353273
 { ... }: {
-  boot.tmp.useTmpfs = true;
+  boot.tmp.cleanOnBoot = true;
 
   systemd.services.nix-daemon = {
     environment.TMPDIR = "/var/tmp";
