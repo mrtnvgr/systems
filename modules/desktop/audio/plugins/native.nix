0d9b4a1f954d37f82c5a5d7cdb900516e8aedb1e
mrtnvgr <martynovegorOF@yandex.ru>
2025-12-02 14:23:31 +0700

    feat(audio/plugins/native): add more

--- modules/desktop/audio/plugins/native.nix
+++ modules/desktop/audio/plugins/native.nix
@@ -1,23 +1,39 @@
-{ pkgs, config, lib, ... }: let
-  inherit (lib) mkEnableOption mkIf;
+{ pkgs, config, lib, user, ... }:
+let
+  # TODO: create vst3 variants for most of these plugins
+  # TODO: auburn-sounds-*
 
   synths = with pkgs; [
     surge-XT-vst3
     vitalium-vst3
+    zynaddsubfx
+    ripplerx
+    # cardinal # fun, but too heavy
+    chow-kick
+    # oxefmsynth
+    # time12, gate12... # TODO: wait for upstream merge # TODO: make -vst3 nurpkgs
   ];
 
   fx = with pkgs; [
-    TAL-plugins-vst3
-    lsp-plugins
-    airwindows
+    lsp-plugins-vst3
+    airwin2rack
+    TAL-plugins-vst2
     neural-amp-modeler-lv2
-    luftikus-vst3
-    LUFSMeter-vst3
+    dragonfly-reverb
+    fire
+    wolf-shaper
+    molot-lite
+    luftikus-vst2
+    LUFSMeter-vst2
+    chow-tape-model
+    chow-centaur
+    chow-phaser
   ];
 
   cfg = config.modules.desktop.audio.plugins;
   isAnyDawInstalled = config._internals.isAnyDawInstalled;
-in {
+in
+{
   options.modules.desktop.audio.plugins = {
     enable = lib.mkOption {
       type = lib.types.bool;
