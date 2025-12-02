c503865a5bac47d7d2f533ff65b9f5c216ce3316
mrtnvgr <martynovegorOF@yandex.ru>
2025-12-02 18:40:36 +0700

    feat(reaper/scripts): patch ColorPalette to use Liberation Sans font instead of Arial, init a configuration

--- modules/desktop/audio/daws/reaper/scripts/ColorPalette.nix
+++ modules/desktop/audio/daws/reaper/scripts/ColorPalette.nix
@@ -1,10 +1,33 @@
-{ inputs, lib, config, ... }: let
+{ inputs, lib, config, pkgs, ... }: let
   cfg = config.modules.desktop.audio.daws.reaper;
+
+  script = pkgs.stdenvNoCC.mkDerivation {
+    name = "ColorPalette-patched";
+
+    src = "${inputs.reascripts}/Various/rodilab_Color palette.lua";
+
+    dontUnpack = true;
+
+    installPhase = ''
+      cp "$src" "$out"
+
+      substituteInPlace "$out" \
+        --replace-fail "Arial" "Liberation Sans"
+    '';
+  };
 in {
   modules.desktop.audio.daws.reaper = lib.mkIf cfg.enable {
     scripts.ColorPalette = {
-      source = "${inputs.reascripts}/Various/rodilab_Color palette.lua";
+      source = script;
       key = "47";
     };
+
+    config."reaper-extstate.ini" = /* dosini */ ''
+      [RODILAB_Color_palette]
+      number_x=12
+      palette_y=1
+      user_y=0
+      background=false
+    '';
   };
 }
