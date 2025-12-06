923f6933f53194a1213a541f5462aafa8fe98648
mrtnvgr <martynovegorOF@yandex.ru>
2025-12-06 15:51:11 +0700

    fix(reaper): use 7.55 reaper (revert when merged upstream)

--- modules/desktop/audio/daws/reaper/default.nix
+++ modules/desktop/audio/daws/reaper/default.nix
@@ -1,7 +1,16 @@
-{ lib, config, inputs, user, ... }:
+{ pkgs, lib, config, inputs, user, ... }:
 let
   desktopCfg = config.modules.desktop;
   cfg = desktopCfg.audio.daws.reaper;
+
+  # TODO: https://github.com/NixOS/nixpkgs/pull/467114
+  reaper = pkgs.callPackage (pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/mrtnvgr/nixpkgs/6d3989c9ace8c33af26e8dad0004100e71b94a82/pkgs/applications/audio/reaper/default.nix";
+    hash = "sha256-NSiMAM2yLJ8S8Dw0IDrajMoqDze8O1Zl6kKOd1vDaXo=";
+  }) {
+    jackLibrary = pkgs.libjack2;
+    ffmpeg = pkgs.ffmpeg_4-headless;
+  };
 in {
   # TODO: use nixpkgs reaimgui when its merged upstream
   # TODO: https://github.com/NixOS/nixpkgs/pull/464016
@@ -18,6 +27,7 @@ in {
 
       programs.reanix = {
         enable = true;
+        package = reaper;
 
         hooks.preRun = ''
           ${lib.optionalString desktopCfg.audio.plugins.wine.enable "wine-audio-plugins-activate"}
