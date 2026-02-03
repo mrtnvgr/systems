9970e80a23fce90304fb6f706ec58c59d11f60ac
mrtnvgr <martynovegorOF@yandex.ru>
2026-02-03 14:15:09 +0700

    chore(server/wireguard): test new rules

--- modules/server/misc/wireguard/tor.nix
+++ modules/server/misc/wireguard/tor.nix
@@ -30,23 +30,31 @@ in {
 
     networking.wg-quick.interfaces.wg0 = {
       postUp = ''
-        ${iptables} -t nat -A PREROUTING -i %i -p tcp --syn -j REDIRECT --to-ports 9040
-        ${iptables} -t nat -A PREROUTING -i %i -p udp --dport 53 -j REDIRECT --to-ports 9053
-        ${iptables} -A INPUT -i %i -p tcp --dport 9040 -j ACCEPT
-        ${iptables} -A INPUT -i %i -p udp --dport 9053 -j ACCEPT
-        ${iptables} -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-        ${iptables} -A FORWARD -i %i -p udp -j DROP
-        ${iptables} -t nat -A POSTROUTING -o ${cfg.publicInterface} -j MASQUERADE
+        ${iptables} -A -t nat PREROUTING -i wg0 -p udp --dport 53 -j DNAT --to-destination 127.0.0.2:9053
+        ${iptables} -A -t nat PREROUTING -i wg0 -p tcp -j DNAT --to-destination 127.0.0.2:9040
+        ${iptables} -A -t nat PREROUTING -i wg0 -p udp -j DNAT --to-destination 127.0.0.2:9040
+        ${iptables} -A OUTPUT -o ${cfg.publicInterface} -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m state --state NEW -j ACCEPT
+        ${iptables} -A OUTPUT -t nat -d 10.192.0.0/10 -p tcp -m tcp -j DNAT --to-destination 127.0.0.2:9040
+        ${iptables} -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
+        ${iptables} -A OUTPUT -m conntrack --ctstate INVALID -j DROP
+        ${iptables} -A OUTPUT -m state --state INVALID -j DROP
+        ${iptables} -A OUTPUT -t nat -o lo -j RETURN
+        ${iptables} -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP
+        ${iptables} -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP
       '';
 
       postDown = ''
-        ${iptables} -t nat -D POSTROUTING -o ${cfg.publicInterface} -j MASQUERADE
-        ${iptables} -D FORWARD -i %i -p udp -j DROP
-        ${iptables} -D FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-        ${iptables} -D INPUT -i %i -p udp --dport 9053 -j ACCEPT
-        ${iptables} -D INPUT -i %i -p tcp --dport 9040 -j ACCEPT
-        ${iptables} -t nat -D PREROUTING -i %i -p udp --dport 53 -j REDIRECT --to-ports 9053
-        ${iptables} -t nat -D PREROUTING -i %i -p tcp --syn -j REDIRECT --to-ports 9040
+        ${iptables} -D -t nat PREROUTING -i wg0 -p udp --dport 53 -j DNAT --to-destination 127.0.0.2:9053
+        ${iptables} -D -t nat PREROUTING -i wg0 -p tcp -j DNAT --to-destination 127.0.0.2:9040
+        ${iptables} -D -t nat PREROUTING -i wg0 -p udp -j DNAT --to-destination 127.0.0.2:9040
+        ${iptables} -D OUTPUT -o ${cfg.publicInterface} -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m state --state NEW -j ACCEPT
+        ${iptables} -D OUTPUT -t nat -d 10.192.0.0/10 -p tcp -m tcp -j DNAT --to-destination 127.0.0.2:9040
+        ${iptables} -D OUTPUT -m state --state ESTABLISHED -j ACCEPT
+        ${iptables} -D OUTPUT -m conntrack --ctstate INVALID -j DROP
+        ${iptables} -D OUTPUT -m state --state INVALID -j DROP
+        ${iptables} -D OUTPUT -t nat -o lo -j RETURN
+        ${iptables} -D OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP
+        ${iptables} -D OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP
       '';
     };
   };
