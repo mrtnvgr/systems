71d98dafb9158c7c25e609f53ecdfe4e12c1ce81
mrtnvgr <martynovegorOF@yandex.ru>
2026-02-25 09:48:44 +0700

    feat(reaper): add jclones jsfx

--- modules/desktop/audio/daws/reaper/jsfx.nix
+++ modules/desktop/audio/daws/reaper/jsfx.nix
@@ -8,5 +8,9 @@ in {
 
     home.file."${jsfx}/chkhld/signal_crusher.jsfx".source = "${inputs.chkhld-jsfx}/Lo-Fi/signal_crusher.jsfx";
     home.file."${jsfx}/chkhld/telephone.jsfx".source = "${inputs.chkhld-jsfx}/Lo-Fi/telephone.jsfx";
+
+    home.file."${jsfx}/jclones/Fattener.jsfx".source = "${inputs.jsfx-clones}/jsfx/JClones_Fattener.jsfx";
+    home.file."${jsfx}/jclones/O3_Maximizer.jsfx".source = "${inputs.jsfx-clones}/jsfx/JClones_O3_Maximizer.jsfx";
+    home.file."${jsfx}/jclones/Classic_Master_Limiter.jsfx".source = "${inputs.jsfx-clones}/jsfx/JClones_Classic_Master_Limiter.jsfx";
   };
 }
