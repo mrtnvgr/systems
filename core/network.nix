142b4946d3678ef1c3a4ef1499967260c00d484f
mrtnvgr <martynovegorOF@yandex.ru>
2026-02-26 23:25:21 +0700

    fix(core): disable dhcpcd on custom dns

--- core/network.nix
+++ core/network.nix
@@ -1,4 +1,13 @@
-{ user, ... }: {
-  networking.networkmanager.enable = true;
-  users.users.${user}.extraGroups = [ "networkmanager" ];
+{ config, user, lib, ... }: {
+  config = lib.mkMerge [
+    { networking.networkmanager.enable = true; }
+    { users.users.${user}.extraGroups = [ "networkmanager" ]; }
+
+    # https://wiki.nixos.org/wiki/NetworkManager#DNS_Management
+    (lib.mkIf (config.networking.nameservers != []) {
+      networking.networkmanager.dns = "none";
+      networking.useDHCP = false;
+      networking.dhcpcd.enable = false;
+    })
+  ];
 }
