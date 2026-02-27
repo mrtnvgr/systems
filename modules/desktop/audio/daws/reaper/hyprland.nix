41c062f5f3560ce1de40da86e9f7d17a7a0a6354
mrtnvgr <martynovegorOF@yandex.ru>
2026-02-27 19:26:10 +0700

    refactor: relocate reaper windowrules to reaper module

--- modules/desktop/audio/daws/reaper/hyprland.nix
+++ modules/desktop/audio/daws/reaper/hyprland.nix
@@ -3,9 +3,15 @@
 in {
   home-manager.users.${user} = lib.mkIf reaper.enable {
     wayland.windowManager.hyprland.extraConfig = ''
-      # REAPER tooltip fix
+      # Tooltip fix
       # https://github.com/hyprwm/Hyprland/issues/2278
       windowrule = no_focus on, match:class REAPER, match:title ^$
+
+      # Hide update popups
+      windowrule = workspace 5 silent, match:class ^REAPER$, match:title ^About.+
+
+      # Improve actions window styling
+      windowrule = center on, stay_focused on, dim_around on, no_anim on, match:class ^REAPER$, match:title ^Actions$
     '';
   };
 }
