2ceed3ff9e42feea1a41ea9fe6409a33e8bd9c80
mrtnvgr <martynovegorOF@yandex.ru>
2026-03-03 23:22:23 +0700

    feat(desktop/neovim): add bg.nvim plugin

--- modules/desktop/apps/neovim/theme/default.nix
+++ modules/desktop/apps/neovim/theme/default.nix
@@ -1,4 +1,15 @@
-{ config, lib, user, ... }: {
+{ config, pkgs, lib, user, ... }: let
+  # TODO: package to nixpkgs
+  bg-nvim = pkgs.vimUtils.buildVimPlugin {
+    name = "bg-nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "typicode";
+      repo = "bg.nvim";
+      rev = "85da2c68409820466753b50c2e6f699517778a17";
+      hash = "sha256-s9m5OgQpehrkIU6bWj70RDEqvSzlxzpih8sRuQrtdC0=";
+    };
+  };
+in {
   imports = [
     ./transparency.nix
     ./overrides.nix
@@ -6,5 +17,7 @@
 
   home-manager.users.${user}.programs.nixvim = lib.mkIf config.modules.desktop.apps.neovim.enable {
     colorschemes.catppuccin.enable = true;
+
+    extraPlugins = [ bg-nvim ];
   };
 }
