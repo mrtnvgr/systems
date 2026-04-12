b6cd7b680ff70dd0397de5ef2efc84715606da2c
mrtnvgr <martynovegorOF@yandex.ru>
2026-04-12 15:00:02 +0700

    chore(core/neovim): remove comment-nvim plugin (see `:h commenting`)

--- core/neovim/plugins.nix
+++ core/neovim/plugins.nix
@@ -1,7 +1,5 @@
 { pkgs, user, ... }: {
   home-manager.users.${user}.programs.nixvim = {
-    plugins.comment.enable = true;
-
     extraPlugins = with pkgs.vimPlugins; [
       # Smart indentation
       indent-o-matic
